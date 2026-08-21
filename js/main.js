e56d22c70012ec1ffe5c6b841d1e77b08ace50be
Vignesh Nadar <149259957+VikkiezDev@users.noreply.github.com>
2026-08-21 16:17:54 +0530

    Add initLeetcodeCounter function call

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
   initMobileNav();
   initNavbarScroll();
   initTypewriter();
+  initLeetcodeCounter();
   renderSkills();
   renderExperience();
   renderProjects();
